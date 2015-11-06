3f2591ae66049c39ee2d1255ef2e5a85a24c7a59
CHRYSTAL BANKS <Chrystal.Banks1@gmail.com>
2015-11-06 09:08:49 -0500

    Initial commit

--- controller/artists_controller.rb
+++ controller/artists_controller.rb
@@ -7,3 +7,13 @@ end
 get "/artists/new" do
   erb :"artists/new"
 end
+
+post "/artists" do
+  @artist = Artist.create(name: params[:name], photo_url: params[:photo_url], nationality: params[:nationality])
+  redirect "/artists"
+end
+
+get "/artists/:id" do
+@artist = Artist.find(params[:id])
+erb :"artists/show"
+end
